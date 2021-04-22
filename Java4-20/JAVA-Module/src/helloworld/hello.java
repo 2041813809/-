44919085e72f04877b662e9b99322064b19848f6
2041813809 <71110483+2041813809@users.noreply.github.com>
2021-04-22 20:29:56 +0800

    Add files via upload

---
+++ Java4-20/JAVA-Module/src/helloworld/hello.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class hello {
+
+    String name;
+
+    int age;
+
+    public void homework() {
+
+        System.out.println(name+","+age);
+    }
+}
+
---
+++ Java4-20/JAVA-Module/src/helloworld/world.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class world {
+    public static void main(String[] args) {
+
+        hello student = new hello();
+
+        student.name = "刘敏";
+        student.age = 18;
+
+        student.homework();
+
+    }
+}
+
